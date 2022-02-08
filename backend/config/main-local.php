4d3e7de4cbaf73a96c51a4b574599679ab7beb81
Omar Hamdan <ohamdan70@gmail.com>
2022-02-09 01:39:40 +0400

    First commit

---
+++ backend/config/main-local.php
@@ -0,0 +1,25 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'FwsV1IHn9Wua9a1VAENevb7O0aLIhwbs',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
---
+++ common/config/codeception-local.php
@@ -0,0 +1,16 @@
+<?php
+
+return yii\helpers\ArrayHelper::merge(
+    require __DIR__ . '/main.php',
+    require __DIR__ . '/main-local.php',
+    require __DIR__ . '/test.php',
+    require __DIR__ . '/test-local.php',
+    [
+        'components' => [
+            'request' => [
+                // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+                'cookieValidationKey' => 'RcwDsz_dzD5cq632onWVV9ZkY8OImC2O',
+            ],
+        ],
+    ]
+);
